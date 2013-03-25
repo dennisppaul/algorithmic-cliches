aa8e25236d010bd2bc89ebc886ad55959079d406
FlowBo <flo.born@gmx.de>
2013-03-25 17:10:51 +0100

    Added first sketch

---
+++ contributions/florianborn/src/de/florianborn/HelloWorld.java
@@ -0,0 +1,17 @@
+package de.florianborn;
+
+import processing.core.PApplet;
+
+public class HelloWorld extends PApplet {
+
+    public void setup() {
+        size(640, 480, OPENGL);
+    }
+
+    public void draw() {
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{HelloWorld.class.getName()});
+    }
+}
