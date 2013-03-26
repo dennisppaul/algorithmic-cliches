3327c09e301f77104362541e3d04abd355773412
psibu <psibu@me.com>
2013-03-26 13:09:29 +0100

    mmm

---
+++ contributions/peterb/src/de/peterb/algorithmicclichees/sketches/Mirror.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.peterb.algorithmicclichees.sketches;
+
+
+import processing.core.PApplet;
+
+
+public class Mirror 
+        extends PApplet {
+
+   public float ellipseSize = random(100,200);
+
+    public void setup() {
+        size(800, 800, OPENGL);
+      
+    }
+
+    public void draw() {
+        background(255);
+        smooth();
+        
+        
+        
+        
+       
+      
+        }
+    
+
+   
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{Mirror.class.getName()});
+    }
+}
