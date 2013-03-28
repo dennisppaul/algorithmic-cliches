26da45fb864b05adeedfebbd58ad7088f26404ee
mathiaslam <mathiaslam@Envy.local>
2013-03-28 10:16:12 +0100

    formatting

--- contributions/mathiaslam/src/de/mathiaslam/algorithmicclichees/Vector2f.java
+++ contributions/mathiaslam/src/de/mathiaslam/algorithmicclichees/Vector2f.java
@@ -6,10 +6,10 @@ class Vector2f {
     float x = 0;
 
     float y = 0;
-    
+
     public Vector2f(float x_, float y_) {
         x = x_;
-        y = y_;       
+        y = y_;
     }
 
     public void add(Vector2f theVector) {
@@ -23,19 +23,19 @@ class Vector2f {
     }
 
     public void multiply(float s) {
-        x*=s;
-        y*=s;
-        
+        x *= s;
+        y *= s;
     }
+
     public void divide(float d) {
-        x/=d;
-        y/=d;
+        x /= d;
+        y /= d;
     }
-    
+
     public float magnitude() {
-        return (float) Math.sqrt(x*x + y*y);
+        return (float) Math.sqrt(x * x + y * y);
     }
-    
+
     public void set(Vector2f theVector) {
         x = theVector.x;
         y = theVector.y;
