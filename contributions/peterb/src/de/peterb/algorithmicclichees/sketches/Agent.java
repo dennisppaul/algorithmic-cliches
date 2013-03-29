d73d58f3d89adbcf07daccf9e0cec22e02fd35d1
psibu <psibu@me.com>
2013-03-29 14:25:17 +0100

    Hornissennest

--- contributions/peterb/src/de/peterb/algorithmicclichees/sketches/Agent.java
+++ contributions/peterb/src/de/peterb/algorithmicclichees/sketches/Agent.java
@@ -24,11 +24,17 @@ public class Agent {
 
     private float myInterval;
 
+    private boolean wait = false;
+
+    private float count = 0;
+
+    private int state = 0;
+
     public Agent() {
         mPosition = new Vector2f();
         mVelocity = new Vector2f();
         mAcceleration = new Vector2f();
-        mMaxVelocity = 4.0f;
+        mMaxVelocity = 2.0f;
         mRadius = 1.0f;
     }
 
@@ -69,44 +75,68 @@ public class Agent {
         mAcceleration.normalize();
     }
 
+    public void setAccelerationToPoint(float x_, float y_) {
+        mAcceleration.x = x_ - mPosition.x;
+        mAcceleration.y = y_ - mPosition.y;
+        mAcceleration.normalize();
+    }
+
     public void setAcceleration(float x_, float y_) {
         mAcceleration.x = x_;
         mAcceleration.y = y_;
         mAcceleration.normalize();
     }
 
-    private float setmyInterval(float i_) {
-        return myInterval;
-    }
 
     /*DISPLAY*/
     public void display(PApplet p) {
+//        myInterval += pDeltaTime;
 
         if (checkMouse(p) == NotArrivedAtMouse) {
-
-            p.strokeWeight(mRadius);
-            p.stroke(0);
-            p.point(mPosition.x, mPosition.y);
-            setAccelerationToMouse(p);
-
         } else if (checkMouse(p) == ArrivedAtMouse) {
             // p.strokeWeight(mRadius);
-            p.stroke(255, 0, 0);
-            p.ellipse(mPosition.x, mPosition.y, mRadius * 5, mRadius * 5);
-            setAcceleration(p.random(-2, 2), p.random(-2, 2));
-
-
         }
 
+
+    }
+    /*STATES*/
+
+    private void wiggle(PApplet p) {
+        //p.stroke(255, 0, 0);
+        p.noStroke();
+        p.fill(0, 30);
+        p.ellipse(mPosition.x, mPosition.y, mRadius * 5, mRadius * 5);
+        setAcceleration(p.random(-2, 2), p.random(-2, 2));
     }
 
+    private void follow(PApplet p) {
+        p.strokeWeight(mRadius);
+        p.stroke(0);
+        p.point(mPosition.x, mPosition.y);
+        setAccelerationToPoint(p.random(0, p.width), p.random(0, p.height));
+    }
     /*MOVE*/
+
     public void update(PApplet p, float pDeltaTime) {
 
-        myInterval += pDeltaTime;
 
         checkWall(p);
-        checkMouse(p);
+        state = checkMouse(p);
+        if (state == 1) {
+            wait = true;
+        }
+        if (wait) {
+            if (myInterval <= 10.0f) {
+                myInterval += pDeltaTime;
+                wiggle(p);
+            } else {
+                myInterval = 0;
+                wait = false;
+                state = 2;
+            }
+        } else {
+            follow(p);
+        }
 
         mVelocity.add(mAcceleration);
         mPosition.add(mVelocity);
@@ -121,7 +151,6 @@ public class Agent {
         }
 
         if (mPosition.y > p.height) {
-//            mVelocity.y *= -1;
             setPosition(mPosition.x, 0);
         } else if (mPosition.y < 0) {
             setPosition(mPosition.x, p.height);
@@ -130,7 +159,7 @@ public class Agent {
     }
 
     private int checkMouse(PApplet p) {
-        float mScaledRadius = (mRadius / mRadius) * 20;
+        float mScaledRadius = (mRadius / mRadius) * 50;
         Vector2f mAB = new Vector2f(p.mouseX - mPosition.x, p.mouseY - mPosition.y);
         float mDistance = mAB.mag();
 
@@ -138,12 +167,8 @@ public class Agent {
             return ArrivedAtMouse;
         } else {
 
-            if (myInterval > 2) {
-//                myInterval = 0;
-                return NotArrivedAtMouse;
-            } else {
-                return ArrivedAtMouse;
-            }
+
+            return NotArrivedAtMouse;
         }
     }
 
@@ -161,5 +186,9 @@ public class Agent {
             mVelocity.mult(mMaxVelocity);
         }
 
+    }
+
+    private void setMyInterval(int x_) {
+        myInterval = x_;
     }
 }
--- contributions/peterb/src/de/peterb/algorithmicclichees/sketches/Run.java
+++ contributions/peterb/src/de/peterb/algorithmicclichees/sketches/Run.java
@@ -11,9 +11,9 @@ public class Run extends PApplet {
 
     public void setup() {
         size(1280, 800, OPENGL);
-        frameRate(60);
 
-//        background(255);
+        frameRate(60);
+//        noCursor();
 
         Vector2f position = new Vector2f(320, 240);
         Vector2f velocity = new Vector2f(2, 1);
@@ -22,29 +22,28 @@ public class Run extends PApplet {
 
     }
 
-    public void mousePressed() {
-        for (int i = 0; i < 100; i++) {
-            Agent a;
-            a = new Agent(new Vector2f(random(0, width), random(0, height)), 2);
-            a.setMaxAcceleration(random(1, 10));
-            agents.add(a);
-        }
-    }
-
+//    public void mousePressed() {
+//        for (int i = 0; i < 1000; i++) {
+//            Agent a;
+//            a = new Agent(new Vector2f(mouseX, mouseY), 5);
+//            a.setMaxAcceleration(random(1, 10));
+//            agents.add(a);
+//        }
+//    }
     public void draw() {
 
         float deltaTime = 1.0f / frameRate;
         background(255);
 
 
-//        if (mousePressed) {
-//            for (int i = 0; i < 10; i++) {
-//                Agent a;
-//                a = new Agent(new Vector2f(width / 2, height / 2), random(1, 10));
-//                a.setMaxAcceleration(random(1, 10));
-//                agents.add(a);
-//            }
-//        }
+        if (mousePressed) {
+            for (int i = 0; i < 10; i++) {
+                Agent a;
+                a = new Agent(new Vector2f(width / 2, height / 2), random(1, 4));
+                a.setMaxAcceleration(random(1, 10));
+                agents.add(a);
+            }
+        }
         for (Agent a : agents) {
             a.update(this, deltaTime);
             a.display(this);
